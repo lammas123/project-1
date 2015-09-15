d004f55a8bb691a45c1be2c49b6a715788990062
Robert Reinapu <robert.reinapu@khk.ee>
2015-09-15 10:40:22 +0300

    avaleht [Delivers #102820876]

---
+++ link.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="pstylesheet.css"/>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body id="lammas">
+<img src="http://www.muurileht.ee/media/Martin-Oja_Foto-Erakogu-e1433944523608-500x376.jpg"/>
+<div>
+    <div id="info"><p> "Tere! Olen Martin. Elan Tartu lähedal maal. Sellest sügisest alates õpin Tartu Kutsehariduskeskuses veebitegemist. Vahel mängin ka plaate DJ Daysleeperi nime all"</p>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ project1.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="project 1.css"/>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<div class="master"> <!--kõik esimese lehe divisionid-->
+    <div id="left"><a href="http://localhost:63342/project%201/link1.php">Informatsiooni minust</a></div>
+    <div id="center"><a href="https://soundcloud.com/maailmamustrid">Kuula minu muusikat siit</a></div>
+    <div id="right"><a href="http://www.vorufolkloor.ee/performer/maailmamustrid">Informatsiooni minu muusika kohta</a></div>
+
+</div>
+<h1 align="center" >Martin "Daysleeper" Oja koduleht</h1>
+</body>
+</html>
